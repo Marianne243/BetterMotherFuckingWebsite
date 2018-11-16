529cda388b054e2549d5c0ea1313b5bc4465f763
Marianne243 <marianne.lisika@live.fr>
2018-11-16 16:44:04 +0100

    Initial commit

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 <script>
 	window.onload = setTimeout(
 		function() {
-            confirm('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please');
-            {
-                txt = "You pressed Ok!"; 
-            window.location = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
-            }
-            else { txt = "You pressed cancel!";
+	    	confirm('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please');
+		    window.location = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
 		}
-	, 10000);
+	, 10000);function ConfirmMessage() {
+		if (confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please")) { 
+			// Clic OK
+			window.location = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+		else (cancel("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please")) {
+			// Clic Fermer
+		}
+		}
+	}
 </script>
