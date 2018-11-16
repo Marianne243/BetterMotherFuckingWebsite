f073a046bd9e944d639f7304728326f082ce12ce
Marianne243 <marianne.lisika@live.fr>
2018-11-16 14:20:57 +0100

    Initial commit

--- main.js
+++ main.js
@@ -2,10 +2,8 @@
 <html>
 <body>
 
-<p>Alert. Wait 10 seconds, and the page will alert "Hey le site xxxvidsxxx est trop bien. Viens dessus stp please".</p>
 
-
-<button onclick="setTimeout(myFunction, 10000);">OK</button>
+setTimeout(myFunction, 10000);
 
 <script>
 function myFunction() {
