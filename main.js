fe72130c2ca6b655ebe6f0933218719a4fa49de3
Marianne243 <marianne.lisika@live.fr>
2018-11-16 16:41:08 +0100

    Initial commit

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 <script>
 	window.onload = setTimeout(
 		function() {
-            if confirm('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please');
+            confirm('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please');
             {
                 txt = "You pressed Ok!"; 
             window.location = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
             }
-            else { txt = "You presses cancel:";
+            else { txt = "You pressed cancel!";
 		}
 	, 10000);
 </script>
