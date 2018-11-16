f113def7b27e8c44121dda740046ca50b815ee07
Marianne243 <marianne.lisika@live.fr>
2018-11-16 16:16:05 +0100

    Initial commit

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 <script>
 var popup = function() {
-    // Boite de dialogue OK-CANCEL
-    popup = confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please")
-    if (popup == Boite de dialogue OK) { 
+        // Boite de dialogue OK-CANCEL
+        popup = confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please")
+if (popup == true) { 
         // Redirection lien
-        window.location = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
-    }
+window.location = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+        }
 };
 setTimeout(popup, 10000)
 
 function couleur() {
-    document.body.style.backgroundColor = "magenta";
+        document.body.style.backgroundColor = "magenta";
 }
 </script>
 
