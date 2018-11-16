2d27effc5080fa0dfeb46e556818d137b8fb559b
Marianne243 <marianne.lisika@live.fr>
2018-11-16 14:09:09 +0100

    Initial comit

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 
 <script 
 if (confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please")) {
-                txt = "You pressed OK!";
+                txt = ("You pressed OK!" <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" onclick="window.confirm('https://www.youtube.com/watch?v=dQw4w9WgXcQ');"> </a>; 
             } else {
                 txt = "You pressed Cancel!";
-            }> </script> 
+            } 
+            function() {
+                delay = 10; 
+  timer = setTimeout(open "Hey le site xxxvidsxxx est trop bien. Viens dessus stp please";
+}   
+            > 
+            
+            
+            </script> 
 
