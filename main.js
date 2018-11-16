0c2f007e357d7c595a53f46b28fbd909bc87d2ab
Marianne243 <marianne.lisika@live.fr>
2018-11-16 15:54:29 +0100

    Initial commit

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
+<script
 var popup = function() {
     // Boite de dialogue OK-CANCEL
     popup = confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please") 
     if (popup == true) { 
         // Redirection lien
-        windows.location = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+        window.location = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
     }
 };
 setTimeout(popup, 10000)
 
 function couleur() {
     document.body.style.backgroundColor = "magenta";
-}
+}>
+</script>
 
