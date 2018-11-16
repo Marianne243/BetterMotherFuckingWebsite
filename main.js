4be230320dd59b84d1dd4e62f7130e0e4f6f90b9
Marianne243 <marianne.lisika@live.fr>
2018-11-16 14:51:37 +0100

    Initial commit

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 
-<script if (confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please!")) {
+if (confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please!")) {
     txt = "You pressed OK!";
 } else {
     txt = "You pressed Cancel!";
-}>
-</script>
+}
