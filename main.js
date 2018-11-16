45fafbcf6fe7f3dfc3df6e32b06910e3f8fd181d
Marianne243 <marianne.lisika@live.fr>
2018-11-16 15:47:25 +0100

    Initial commit

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-<script>var popup = function() {
+var popup = function() {
     // Boite de dialogue OK-CANCEL
     popup = confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please") 
     if (popup == true) { 
@@ -10,4 +10,5 @@ setTimeout(popup, 10000)
 
 function couleur() {
     document.body.style.backgroundColor = "magenta";
-}</script>
+}
+
