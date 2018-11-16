e8a098f0169ebcb42e3a4c6a1e846cf652d9f04b
Marianne243 <marianne.lisika@live.fr>
2018-11-16 15:57:46 +0100

    Initial commit

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 var popup = function() {
     // Boite de dialogue OK-CANCEL
     popup = confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please") 
-    if (popup == true) { 
+    if (popup == OK) { 
         // Redirection lien
         window.location = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
     }
